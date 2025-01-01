eaab533c3fc1216cd817789b90ee53ae628cfbcb
Simge Mede <109466550+simgemede@users.noreply.github.com>
2025-01-01 16:28:13 +0300

    .

--- mobile_to_do_planner/lib/UI/theme.dart
+++ mobile_to_do_planner/lib/UI/theme.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Themes {
-  static final aydinlik = ThemeData(
-      primaryColor: Colors.blue, // Üst renk mavi
-      brightness: Brightness.light // Uygulamanın açık tema
-      );
+  static final aydinlik =
+      ThemeData(brightness: Brightness.light // Uygulamanın açık tema
+          );
 
-  static final karanlik = ThemeData(
-      primaryColor: const Color.fromARGB(255, 44, 44, 44),
-      brightness: Brightness.dark);
+  static final karanlik = ThemeData(brightness: Brightness.dark);
 }
--- mobile_to_do_planner/lib/services/theme_services.dart
+++ mobile_to_do_planner/lib/services/theme_services.dart
@@ -7,13 +7,15 @@ import 'package:get_storage/get_storage.dart';
 class TemaServisi {
   final _temaKutusu =
       GetStorage(); // İcinde saklamak icin getstorage örnegi olusturdum.
-  final _anahtar = 'karanlikMod'; // Depolamada kullanilacak anahtar
-
+  final _anahtar = 'karanlikModMu'; // Depolamada kullanilacak anahtar
+  _temayiKutuyaKaydet(bool karanlikModMu) =>
+      _temaKutusu.write(_anahtar, karanlikModMu);
   bool _temaKutusundanAl() => _temaKutusu.read(_anahtar) ?? false;
   ThemeMode get temaModu => _temaKutusundanAl()
       ? ThemeMode.dark
       : ThemeMode.light; // true ise dark döner
   void temaDegistir() {
     Get.changeThemeMode(_temaKutusundanAl() ? ThemeMode.light : ThemeMode.dark);
+    _temayiKutuyaKaydet(!_temaKutusundanAl()); // Temanın tersi
   }
 }
